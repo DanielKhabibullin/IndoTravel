1067a57099717afcb4cad2331a6af5d2f91446bf
Daniel Khabibullin <i6unny@gmail.com>
2023-03-09 00:01:11 +0500

    Update timer

--- script/modules/timer.js
+++ script/modules/timer.js
@@ -1,5 +1,7 @@
 import {countDays, countHours, countMinutes,
-	heroText, heroTimer, unitsDays, unitsHours, unitsMinutes} from './const.js';
+	countSeconds, heroText, heroTimer, itemDays,
+	itemSeconds, unitsDays, unitsHours,
+	unitsMinutes, unitsSeconds} from './const.js';
 import {addZero} from './utils.js';
 
 // возвращает число и слово
@@ -11,7 +13,10 @@ const declOfNum = (n, titles) => titles[n % 10 === 1 &&
 export const timer = () => {
 	const getTimeRemaining = () => {
 		const deadline = heroTimer.getAttribute('data-timer-deadline');
-		const dateStop = new Date(deadline).getTime();
+		// const dateStopLocal = new Date(deadline).getTime();
+		const gmt3 = (new Date().getTimezoneOffset() + 180) * 60 * 1000;
+		const dateStop = new Date(deadline).getTime() + gmt3;
+
 		const dateNow = Date.now();
 		const timeRemaining = dateStop - dateNow;
 
@@ -25,21 +30,22 @@ export const timer = () => {
 
 	const start = () => {
 		const timer = getTimeRemaining();
-		// timer.timeRemaining = 0;
+
 		countDays.textContent = timer.days;
 		countHours.textContent = addZero(timer.hours);
 		countMinutes.textContent = addZero(timer.minutes);
-		// countSeconds.textContent = addZero(timer.seconds);
+		countSeconds.textContent = addZero(timer.seconds);
+		itemSeconds.style.display = 'none';
 
-		// const s = declOfNum(timer.seconds, ['секунда', 'секунды', 'секунд']);
+		const s = declOfNum(timer.seconds, ['секунда', 'секунды', 'секунд']);
 		const m = declOfNum(timer.minutes, ['минута', 'минуты', 'минут']);
 		const h = declOfNum(timer.hours, ['час', 'часа', 'часов']);
 		const d = declOfNum(timer.days, ['день', 'дня', 'дней']);
 
-		// unitsSeconds.textContent = `${s}`;
-		unitsMinutes.textContent = ` ${m}`;
-		unitsHours.textContent = ` ${h}`;
-		unitsDays.textContent = ` ${d}`;
+		unitsSeconds.textContent = `${s}`;
+		unitsMinutes.textContent = `${m}`;
+		unitsHours.textContent = `${h}`;
+		unitsDays.textContent = `${d}`;
 
 		// if (timer.timeRemaining > 0) {
 		// setTimeout(start, 60000);
@@ -48,9 +54,16 @@ export const timer = () => {
 		// heroText.remove();
 		// }
 
-		const intervalId = setTimeout(start, 60000);
+		const timerId = setTimeout(start, 60000);
+
+		if (timer.timeRemaining < 1000 * 60 * 60 * 24) {
+			clearTimeout(timerId);
+			setTimeout(start, 1000);
+			itemDays.style.display = 'none';
+			itemSeconds.style.display = 'flex';
+		}
 		if (timer.timeRemaining <= 0) {
-			clearTimeout(intervalId);
+			clearTimeout();
 			heroTimer.remove();
 			heroText.remove();
 		}
