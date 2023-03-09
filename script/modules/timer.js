28ca8ca8d33b066cc152e24912f4dcd344b397f9
Daniel Khabibullin <i6unny@gmail.com>
2023-03-09 20:21:02 +0500

    Add fix

--- script/modules/timer.js
+++ script/modules/timer.js
@@ -33,11 +33,12 @@ export const timer = () => {
 			`;
 
 			const getTimeRemaining = () => {
-				// const deadline = heroTimer.getAttribute('data-timer-deadline');
-				// const dateStopLocal = new Date(deadline).getTime();
-				const gmt3 = (new Date().getTimezoneOffset() + 180) * 60 * 1000;
-				const dateStop = new Date(deadline).getTime() + gmt3;
+				const dateNowLocal = new Date();
+				const hoursNow = dateNowLocal.getHours();
+				const hoursNowUTC = dateNowLocal.getUTCHours();
+				const GMT3 = (hoursNow - (hoursNowUTC + 3)) * 60 * 60 * 1000;
 
+				const dateStop = new Date(deadline).getTime() + GMT3;
 				const dateNow = Date.now();
 				const timeRemaining = dateStop - dateNow;
 
@@ -51,12 +52,14 @@ export const timer = () => {
 
 			const start = () => {
 				const timer = getTimeRemaining();
-				const countDays = document.querySelector('.timer__count_days');
-				const countHours = document.querySelector('.timer__count_hours');
-				const countMinutes = document.querySelector('.timer__count_minutes');
-				const countSeconds = document.querySelector('.timer__count_seconds');
-				const itemSeconds = document.querySelector('.timer__item_seconds');
-				const itemDays = document.querySelector('.timer__item_days');
+				const countDays = timerElement.querySelector('.timer__count_days');
+				const countHours = timerElement.querySelector('.timer__count_hours');
+				const countMinutes = timerElement
+					.querySelector('.timer__count_minutes');
+				const countSeconds = timerElement
+					.querySelector('.timer__count_seconds');
+				const itemSeconds = timerElement.querySelector('.timer__item_seconds');
+				const itemDays = timerElement.querySelector('.timer__item_days');
 
 				countDays.innerHTML = timer.days;
 				countHours.innerHTML = addZero(timer.hours);
@@ -68,33 +71,28 @@ export const timer = () => {
 				const m = declOfNum(timer.minutes, ['минута', 'минуты', 'минут']);
 				const h = declOfNum(timer.hours, ['час', 'часа', 'часов']);
 				const d = declOfNum(timer.days, ['день', 'дня', 'дней']);
-				const unitsDays = document.querySelector('.timer__units_days');
-				const unitsHours = document.querySelector('.timer__units_hours');
-				const unitsMinutes = document.querySelector('.timer__units_minutes');
-				const unitsSeconds = document.querySelector('.timer__units_seconds');
+				const unitsDays = timerElement.querySelector('.timer__units_days');
+				const unitsHours = timerElement.querySelector('.timer__units_hours');
+				const unitsMinutes = timerElement
+					.querySelector('.timer__units_minutes');
+				const unitsSeconds = timerElement
+					.querySelector('.timer__units_seconds');
 
 				unitsSeconds.textContent = `${s}`;
 				unitsMinutes.textContent = `${m}`;
 				unitsHours.textContent = `${h}`;
 				unitsDays.textContent = `${d}`;
 
-				// if (timer.timeRemaining > 0) {
-				// setTimeout(start, 60000);
-				// } else {
-				// heroTimer.remove();
-				// heroText.remove();
-				// }
-
-				const timerId = setTimeout(start, 60000);
+				let timerId = setTimeout(start, 60000);
 
 				if (timer.timeRemaining < 1000 * 60 * 60 * 24) {
 					clearTimeout(timerId);
-					setTimeout(start, 1000);
+					timerId = setTimeout(start, 1000);
 					itemDays.style.display = 'none';
 					itemSeconds.style.display = 'flex';
 				}
 				if (timer.timeRemaining <= 0) {
-					clearTimeout();
+					clearTimeout(timerId);
 					heroTimer.remove();
 					heroText.remove();
 				}
